2e6512ff4448551501a36d94ee69a50734119f43
jacquesdubois <jacquesadubois@gmail.com>
2021-10-26 12:57:16 -0700

    Created the Live Anywhere homepage section, which uses a MediumCard component; additionally downloaded and imported the tailwind-hide-scrollbar package to make navigation in this section more aesthetically simple/pleasing

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -16,8 +17,6 @@ export default function Home({ exploreData }) {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
-
-          {/* Pull some data from a server - API endpoints */}
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
@@ -28,6 +27,19 @@ export default function Home({ exploreData }) {
               />
             ))}
           </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+            <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+              {cardsData?.map(({img, title}) => (
+                <MediumCard
+                  key={img}
+                  img={img}
+                  title={title}
+                />
+              ))}
+            </div>
         </section>
       </main>
     </div>
@@ -40,9 +52,15 @@ export async function getStaticProps() {
       return res.json();
     });
 
+  const cardsData = await fetch('https://links.papareact.com/zp1')
+  .then( (res) => {
+    return res.json();
+  });
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
